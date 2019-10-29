dcaa98dbdb007005529409a658ff6893719957ec
You <123@example.com>
2019-10-29 21:26:51 +0800

    111

--- ht_prj/routes/dangdang.js
+++ ht_prj/routes/dangdang.js
@@ -13,5 +13,50 @@ router.get('/banner', function(req, res, next) {
     res.send(arr);
 });
 
+router.get('/cateList',function(req,res,next){
+    var arr=[
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05567113407955289.png',
+            txt:'全球优选'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05567113407955289.png',
+            txt:'兴农扶贫'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05567113407955289.png',
+            txt:'兴农扶贫'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05567113730153863.png',
+            txt:'工厂直供'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05567113870067331.png',
+            txt:'淘库好店'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05525029655588000.png',
+            txt:'发现好货'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05525033900202042.png',
+            txt:'限量抢购'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05591315728041790.png',
+            txt:'限时秒杀'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05591315064860042.png',
+            txt:'红包活动'
+        },
+        {
+            img:'https://www.taokubuy.com/data/upload/shop/adv/05279588135542058.png',
+            txt:'签到有奖'
+        }
+    ]
+    res.send(arr);
+})
 
 module.exports = router;
