25f84afe3df461fcec3d1b79fa08b45d36a06230
haha Name <2353044658@qq.com>
2019-10-29 21:42:02 +0800

    add car

---
+++ prj/src/components/car/compnents/carr.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="tou">
+    <div class="touz">
+      <a href="#">
+        <i class="iconfont icon-changyongtubiao-xianxingdaochu-zhuanqu-"></i>
+      </a>
+    </div>
+    <div class="touzh">
+      <h1>购物车</h1>
+    </div>
+    <div class="touy">
+      <a href="#">
+        <i class="iconfont icon-caidan"></i>
+      </a>
+    </div>
+    <navs></navs>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.tou {
+  background: rgba(248, 248, 248, 0.95);
+  width: 100%;
+  height: 88px;
+  display: flex;
+}
+.touz {
+  float: left;
+  //   display: block;
+  width: 88px;
+  height: 88px;
+}
+.touz a {
+  // display: block;
+  width: 86px;
+  height: 86px;
+}
+.icon-changyongtubiao-xianxingdaochu-zhuanqu- {
+  font-size: 45px;
+  line-height: 86px;
+  margin-left: 15px;
+}
+.touzh {
+  display: flex;
+  flex: 1;
+  line-height: 86px;
+  width: 106px;
+  height: 86px;
+  align-items: center;
+  justify-content: center;
+}
+.touzh h1 {
+  // flex: 1;
+  font-size: 35px;
+  font-weight: 550;
+}
+.touy {
+  float: right;
+  width: 86px;
+  height: 86px;
+}
+.touy a {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.icon-caidan {
+  font-size: 45px;
+  line-height: 86px;
+  color: #969696;
+}
+.nav{
+    // display: none;
+    position: absolute;
+    right: 0;
+}
+</style>
+<script>
+import navs from "./nav.vue";
+export default {
+  components: {
+    navs
+  }
+};
+</script>
---
+++ prj/src/components/car/compnents/nav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="nav">
+    <span></span>
+    <ul>
+      <li>
+        <a href="#">
+          <i class="iconfont icon-shouye"></i>首页
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="iconfont icon-sousuo"></i>搜索
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="iconfont icon-leimupinleifenleileibie"></i>分类
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="iconfont icon-xiaoxi"></i>消息
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="iconfont icon-wode"></i>我的商城
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<style lang="less">
+.nav {
+  padding-top: 88px;
+  box-sizing: border-box;
+  font-size: 30px;
+}
+//     span {
+//       width: 10px;
+//       height: 10px;
+//       border-left: 50px solid transparent;
+//       border-right: 50px solid transparent;
+//       border-bottom: 100px solid red;
+// }
+ul {
+  width: 250px;
+  height: 485px;
+}
+li {
+  width: 100%;
+  height: 95px;
+}
+li a{
+    width: 100%;
+    height: 100%;
+}
+i{
+    width: 36px;
+    height: 36px;
+}
+</style>
