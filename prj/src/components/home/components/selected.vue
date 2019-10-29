f48a58191752d0097b14e6944cefb963118edcd5
You <123@example.com>
2019-10-29 21:23:01 +0800

    aaa

---
+++ prj/src/components/home/components/selected.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <img src="https://www.taokubuy.com/data/upload/mobile/special/s0/s0_06227652599101434.jpg" alt="">
+    <div class="top">
+        <i></i>
+        <div>限量抢购
+            <span>更多</span>
+        </div>
+        
+    </div>
+</div>
+</template>
+<style lang="less" scoped>
+.top{
+    display: flex;
+    font-size: 38px;
+    i{
+        height: 36px;
+        width: 8px;
+        background: chocolate;
+        margin: 10px
+    }
+    div{
+        width: 799px;
+        span{
+            float: right
+        }
+    }
+}
+</style>
