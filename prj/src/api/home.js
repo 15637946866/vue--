1cd75625fa678434d0e39ae5c6b09ac47959a69c
You <123@example.com>
2019-10-30 21:12:49 +0800

    首页

--- prj/src/api/home.js
+++ prj/src/api/home.js
@@ -6,3 +6,15 @@ export function getBanner(){
 export function getCateList(){
     return axios.get('dang/cateList')
 }
+export function getSelect(){
+    return axios.get('/dang/select')
+}
+export function getSeackill(){
+    return axios.get('/dang/select')
+}
+export function getBrand(){
+    return axios.get('/dang/brand')
+}
+export function getShopper(){
+    return axios.get('/dang/shopper')
+}
